ed26844b040962cbfdb5a863b7d75d5ab4b52ea0
And4Web <anands2807@gmail.com>
2022-01-25 10:10:12 -0800

    table added.

--- src/Main.js
+++ src/Main.js
@@ -9,5 +9,24 @@ export default function Main() {
       .catch((err) => console.log(err));
   }, []);
 
-  return <h1>CovidStats in Main</h1>;
+  return (
+    <div>
+      <h1>Covid Tracker</h1>
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <table className="table table-primary">
+            <thead>
+              <tr>
+                <th>Country</th>
+                <th>Total Cases</th>
+                <th>Active</th>
+                <th>Recovered</th>
+                <th>Deaths</th>
+              </tr>
+            </thead>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
