5e78bf0a9ead95defb34cb97d4037c3e9db8df50
And4Web <anands2807@gmail.com>
2022-01-25 12:59:35 -0800

    table created and data filled

--- src/Main.js
+++ src/Main.js
@@ -2,19 +2,36 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Main() {
+  const [covidData, setCovidData] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://api.covid19api.com/summary")
-      .then((res) => console.log(res.data))
+      .then((res) => {
+        console.log(res.data.Countries);
+        setCovidData(res.data.Countries);
+      })
       .catch((err) => console.log(err));
   }, []);
+
+  const tableData = covidData.map((obj) => {
+    return (
+      <tr>
+        <td>{obj.Country}</td>
+        <td>{obj.TotalConfirmed}</td>
+        <td>{obj.TotalConfirmed - obj.TotalRecovered}</td>
+        <td>{obj.TotalRecovered}</td>
+        <td>{obj.TotalDeaths}</td>
+      </tr>
+    );
+  });
 
   return (
     <div>
       <h1>Covid Tracker</h1>
       <div className="row justify-content-center">
         <div className="col-md-8">
-          <table className="table table-primary">
+          <table className="table table-secondary">
             <thead>
               <tr>
                 <th>Country</th>
@@ -24,6 +41,7 @@ export default function Main() {
                 <th>Deaths</th>
               </tr>
             </thead>
+            <tbody>{tableData}</tbody>
           </table>
         </div>
       </div>
